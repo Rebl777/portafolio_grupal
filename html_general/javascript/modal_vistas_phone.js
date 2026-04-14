e6990eed5c8e9ace21bc9ec931192ad006f05b64
Rebl777 <Rebecalucero38@gmail.com>
2026-04-14 14:46:26 -0600

    se agrego el js, se quitaron clases y se agregaron botones en alectronica

---
+++ html_general/javascript/modal_vistas_phone.js
@@ -0,0 +1,26 @@
+let proyectos = document.querySelectorAll(".imagen_proyecto");
+let modal = document.getElementById("modal_proyecto");
+
+let modalImg = modal.querySelector(".modal_img");
+let modalTitulo = modal.querySelector(".modal_titulo");
+let modalDesc = modal.querySelector(".modal_desc");
+
+// abrir modal
+proyectos.forEach(proyecto => {
+    proyecto.addEventListener("click", function() {
+        let img = proyecto.querySelector("img");
+        let titulo = proyecto.querySelector(".titulo").textContent;
+        let oculto = proyecto.querySelector(".oculto").textContent;
+
+        modalImg.src = img.src;
+        modalTitulo.textContent = titulo;
+        modalDesc.textContent = oculto;
+
+        modal.showModal();
+    });
+});
+
+// cerrar modal
+modal.querySelector(".cerrar").addEventListener("click", () => {
+    modal.close();
+});
