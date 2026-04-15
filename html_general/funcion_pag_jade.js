a0a59d3d0c7a084c85da80e18eb43c4c65495b77
Jade Diaz <mokythekiller@gmail.com>
2026-04-15 11:32:38 -0600

    pagina personal de jade

--- html_general/funcion_pag_jade.js
+++ html_general/funcion_pag_jade.js
@@ -1,16 +1,17 @@
 let correo = document.getElementById("correo");
 let correo_flotante = document.getElementById("correo_flotante");
 let salida = document.getElementById("salida") ;
+let pagina_completa = document.getElementById("pagina_completa");
 
 function entrar (){
     correo_flotante.classList.remove("oculto");
+    pagina_completa.classList.add("oculto");
 }
 
 function salir (){
     correo_flotante.classList.add("oculto");
+    pagina_completa.classList.remove("oculto");
 }
 
 correo.addEventListener("click", entrar);
 salida.addEventListener("click", salir);
-
-console.log("JS conectado");
